365943768675bbb3a458e439209dc6c8bc068232
Ann Teneneva <a.teneneva@taxi-master.com>
2019-06-19 11:48:56 +0400

    * GUI form template with JavaFX.

---
+++ src/main/java/ru/ndker/piano/model/SearchResult.java
@@ -0,0 +1,11 @@
+package ru.ndker.piano.model;
+
+import java.util.Date;
+
+public class SearchResult {
+    private long id;
+    private Date date;
+    private String title;
+    private String author;
+    private String sourceLink;
+}
